daee31493726b97474dc5b528840cb57972842d2
AtanasNikolov1 <atanasnik989@gmail.com>
2024-03-18 22:58:42 +0200

    Create and connect modules module

--- timetrend-store/src/app/app.module.ts
+++ timetrend-store/src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { ModulesModule } from './modules/modules.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, SharedModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    ModulesModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
