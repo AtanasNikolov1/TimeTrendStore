87b2e7c6e7dbefcdaea8b79bc4e38931674a94ba
AtanasNikolov1 <atanasnik989@gmail.com>
2024-03-19 11:10:41 +0200

    Create the home module

---
+++ timetrend-store/src/app/modules/home/home.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeroSectionComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
