5d75e1c7badd794be21358c7dc6adedb7a74ee80
AtanasNikolov1 <atanasnik989@gmail.com>
2024-03-25 23:29:54 +0200

    Fix the auth module

--- timetrend-store/src/app/modules/auth/auth.module.ts
+++ timetrend-store/src/app/modules/auth/auth.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { RegisterFormComponent } from './components/register-form/register-form.component';
-import { ContainerComponent } from './components/container/container.component';
-import { LoginWrapperComponent } from './components/login-wrapper/login-wrapper.component';
-import { RegisterWrapperComponent } from './components/register-wrapper/register-wrapper.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { CoreModule } from 'src/app/core/core.module';
 
 @NgModule({
   declarations: [
     LoginFormComponent,
     RegisterFormComponent,
-    ContainerComponent,
-    LoginWrapperComponent,
-    RegisterWrapperComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
-  imports: [CommonModule],
+  imports: [CommonModule, CoreModule],
+  exports: [LoginComponent, RegisterComponent],
 })
 export class AuthModule {}
