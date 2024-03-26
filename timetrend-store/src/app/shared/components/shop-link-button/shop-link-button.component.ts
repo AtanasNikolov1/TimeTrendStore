136e2110990647e8830e9b61457b0279487fcf5c
AtanasNikolov1 <atanasnik989@gmail.com>
2024-03-26 13:24:59 +0200

    Update the hero section component style

---
+++ timetrend-store/src/app/shared/components/shop-link-button/shop-link-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-link-button',
+  templateUrl: './shop-link-button.component.html',
+  styleUrls: ['./shop-link-button.component.css']
+})
+export class ShopLinkButtonComponent {
+
+}
