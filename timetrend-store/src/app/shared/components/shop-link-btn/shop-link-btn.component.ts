afef6492b689270bd6c592eab6d13dbf4eb1326a
AtanasNikolov1 <atanasnik989@gmail.com>
2024-04-11 09:11:15 +0300

    Add new shop link button component

---
+++ timetrend-store/src/app/shared/components/shop-link-btn/shop-link-btn.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-link-btn',
+  templateUrl: './shop-link-btn.component.html',
+  styleUrls: ['./shop-link-btn.component.css'],
+})
+export class ShopLinkBtnComponent {}
