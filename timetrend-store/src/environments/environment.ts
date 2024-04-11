d3fee3a5f39e95cac346594268752169da6a86ee
AtanasNikolov1 <atanasnik989@gmail.com>
2024-04-11 09:35:15 +0300

    Add the configuration of the firebase

---
+++ timetrend-store/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyDnm8Yyb3HF0HWZMPh07A_j1e5Xaq4jDRE',
+    authDomain: 'timetrend-api.firebaseapp.com',
+    projectId: 'timetrend-api',
+    storageBucket: 'timetrend-api.appspot.com',
+    messagingSenderId: '425514474605',
+    appId: '1:425514474605:web:3005bc60fa3ccdc99bf364',
+  },
+};
