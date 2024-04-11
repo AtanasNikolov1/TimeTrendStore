f06fdf2ec727ad0afc6b26975d5d8bf4b7319797
AtanasNikolov1 <atanasnik989@gmail.com>
2024-04-11 09:21:14 +0300

    Update the shared module file

--- timetrend-store/src/app/shared/shared.module.ts
+++ timetrend-store/src/app/shared/shared.module.ts
@@ -1,9 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ShopLinkBtnComponent } from './components/shop-link-btn/shop-link-btn.component';
+import { RouterModule } from '@angular/router';
+import { ProductComponent } from './components/product/product.component';
+import { LoadingComponent } from './components/loading/loading.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
-  exports: [],
+  declarations: [
+    ShopLinkBtnComponent,
+    ProductComponent,
+    LoadingComponent,
+    ErrorComponent,
+  ],
+  imports: [CommonModule, RouterModule],
+  exports: [
+    ShopLinkBtnComponent,
+    ProductComponent,
+    LoadingComponent,
+    ErrorComponent,
+  ],
 })
 export class SharedModule {}
