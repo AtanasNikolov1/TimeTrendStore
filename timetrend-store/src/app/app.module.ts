65cb89f2dcb07e592dca7ea863e4e4f21635c6df
AtanasNikolov1 <atanasnik989@gmail.com>
2024-04-11 18:37:44 +0300

    Connect firebase to the app

--- timetrend-store/src/app/app.module.ts
+++ timetrend-store/src/app/app.module.ts
@@ -5,10 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ModulesModule } from './modules/modules.module';
 import { CoreModule } from './core/core.module';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ModulesModule, CoreModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ModulesModule,
+    CoreModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    FormsModule,
+    SharedModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
