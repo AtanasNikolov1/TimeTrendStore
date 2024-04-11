10907bf1bfbb59dec0bbfad7d34817b359416c5c
AtanasNikolov1 <atanasnik989@gmail.com>
2024-04-11 18:32:53 +0300

    Add the address interface

---
+++ timetrend-store/src/app/modules/account/interfaces/address.interface.ts
@@ -0,0 +1,10 @@
+export interface Address {
+  id: string;
+  addressNickname: string;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  country: string;
+  city: string;
+  address: string;
+}
