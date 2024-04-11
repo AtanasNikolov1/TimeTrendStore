85cd881816e934a0ad8aaf12ef2707f402aebe0f
AtanasNikolov1 <atanasnik989@gmail.com>
2024-04-11 11:52:37 +0300

    Modify the email validation

--- timetrend-store/src/app/modules/auth/validators/emailValidator.ts
+++ timetrend-store/src/app/modules/auth/validators/emailValidator.ts
@@ -3,7 +3,7 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 export function emailValidator(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
     const email = control.value as string;
-    const pattern = /^[a-zA-Z]{6,}@(?:gmail\.com|abv\.bg)$/;
+    const pattern = /^[a-zA-Z]{2,}@(?:gmail\.com|abv\.bg)$/;
     if (!pattern.test(email)) {
       return { invalidEmail: true };
     }
