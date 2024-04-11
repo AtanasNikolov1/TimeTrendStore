d4a6cfa18c2a51770f5258aec9c46e89a908beea
AtanasNikolov1 <atanasnik989@gmail.com>
2024-04-11 09:02:47 +0300

    Update the core module file

--- timetrend-store/src/app/core/core.module.ts
+++ timetrend-store/src/app/core/core.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { RouterModule } from '@angular/router';
+import { ErrorPageComponent } from './pages/404/404.component';
+import { AccountGuard } from './guards/account-guard.guard';
+import { AuthGuard } from './guards/auth-guard.guard';
+import { AuthService } from './services/auth.service';
 
 @NgModule({
-  declarations: [NavigationComponent, FooterComponent],
+  declarations: [NavigationComponent, FooterComponent, ErrorPageComponent],
   imports: [CommonModule, RouterModule],
-  exports: [NavigationComponent, FooterComponent],
+  providers: [AccountGuard, AuthGuard, AuthService],
+  exports: [NavigationComponent, FooterComponent, ErrorPageComponent],
 })
 export class CoreModule {}
