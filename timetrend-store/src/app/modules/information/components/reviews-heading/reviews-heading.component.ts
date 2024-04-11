f1b501b15ad6f4de69956ae69e3645739d391319
AtanasNikolov1 <atanasnik989@gmail.com>
2024-04-11 18:27:06 +0300

    Add the reviews heading component

---
+++ timetrend-store/src/app/modules/information/components/reviews-heading/reviews-heading.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reviews-heading',
+  templateUrl: './reviews-heading.component.html',
+  styleUrls: ['./reviews-heading.component.css']
+})
+export class ReviewsHeadingComponent {
+
+}
