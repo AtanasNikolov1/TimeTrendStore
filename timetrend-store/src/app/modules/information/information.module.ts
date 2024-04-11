af61d6c501c290617831c7fa66dbb924c778e4d7
AtanasNikolov1 <atanasnik989@gmail.com>
2024-04-11 18:29:26 +0300

    Update the information module

--- timetrend-store/src/app/modules/information/information.module.ts
+++ timetrend-store/src/app/modules/information/information.module.ts
@@ -3,10 +3,18 @@ import { CommonModule } from '@angular/common';
 import { AboutComponent } from './pages/about/about.component';
 import { ReviewsComponent } from './pages/reviews/reviews.component';
 import { CoreModule } from 'src/app/core/core.module';
+import { ReviewsHeadingComponent } from './components/reviews-heading/reviews-heading.component';
+import { ReviewsBoxesComponent } from './components/reviews-boxes/reviews-boxes.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
-  declarations: [AboutComponent, ReviewsComponent],
-  imports: [CommonModule, CoreModule],
+  declarations: [
+    AboutComponent,
+    ReviewsComponent,
+    ReviewsHeadingComponent,
+    ReviewsBoxesComponent,
+  ],
+  imports: [CommonModule, CoreModule, SharedModule],
   exports: [AboutComponent, ReviewsComponent],
 })
 export class InformationModule {}
